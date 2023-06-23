2b99d9da0732460b71391c9559435a1140414751
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-06-23 16:01:46 +0300

    Initial Commit

---
+++ MyToDosSwiftUI/MyToDosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosSwiftUIApp.swift
+//  MyToDosSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
